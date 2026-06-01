4cd65f1968cc4093d333ee36a72c10bca777797a
Brayden Youngberg <bjyberg1@gmail.com>
2026-06-01 10:49:50 +0200

    feat(config): Add some sensible/explicit defaults & additions. Search, fonts from CASP, md renderer
      Add fonts from the casp style sheet, have base and site be pulled from env vars to allow deploy on Cloudflare, ghpages, or elsewhere. Copy over some md config as a base but can be chagned in the future. It seems the plan for astro is to migrate to satteri as default md renderer, so using it now, but easy to swap back to unified if we need their plugins... Added a few other quality of life things & defaults

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,53 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
+import { satteri } from "@astrojs/markdown-satteri";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+
+import pagefind from "astro-pagefind";
+
+// Different deploy opts depending on the env (gh pages vs cloudflare)
+const base = process.env.BASE_PATH ?? "/";
+const site = process.env.SITE_URL;
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()]
+	site,
+	base,
+	integrations: [mdx(), sitemap(), pagefind()],
+	prefetch: true,
+	output: "static",
+	trailingSlash: "never",
+
+	markdown: {
+		processor: satteri({
+			features: { directive: true },
+		}),
+		shikiConfig: {
+			themes: {
+				light: "github-light",
+				dark: "github-dark",
+			},
+		},
+	},
+
+	fonts: [
+		{
+			name: "Noto Sans",
+			cssVariable: "--font-noto-sans",
+			provider: fontProviders.fontsource(),
+			weights: [300, 400, 700],
+			styles: ["normal", "italic"],
+			subsets: ["latin"],
+		},
+		{
+			name: "Noto Serif",
+			cssVariable: "--font-noto-serif",
+			provider: fontProviders.fontsource(),
+			weights: [400, 700],
+			styles: ["normal", "italic"],
+			subsets: ["latin"],
+		},
+	],
 });
